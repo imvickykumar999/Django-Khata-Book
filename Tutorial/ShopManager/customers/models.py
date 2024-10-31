2d73921a3afbe32bf18ae6b530cbfb4f0c885ae1
Vicky Kumar <imvickykumar999@gmail.com>
2024-10-31 12:48:46 +0530

    first commit

---
+++ Tutorial/ShopManager/customers/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class Purchase(models.Model):
+    customer = models.ForeignKey(Customer, related_name='purchases', on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    purchase_date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.item_name} - {self.customer.name}"
+    
